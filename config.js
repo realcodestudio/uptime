ee221bb7c970ea791eba3a082aa112ba14ed8be1
RCBS <erer2323@qq.com>
2025-04-29 21:46:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,24 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Services Status',
+  SiteName: 'Bousai CN Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur2822193-a62f0194c6f004f44686f6bb',
-    //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
-    //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
-    //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
   ],
 
   // 日志天数
-  CountDays: 60,
+  CountDays: 31,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: '主页',
+      text: 'BS主页',
       url: 'https://bousai.cn/'
     },    
     {
